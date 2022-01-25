ec9651e59e064e6fb6cbeeec822f06ed380dc84c
hakosaj <jussi.hakosalo@aalto.fi>
2022-01-25 07:19:51 +0000

    feat: bashing

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -12,6 +12,10 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("imperial")) {
             return "Imperial College is a university in London";
         }
-        return "";
+        if (query.toLowerCase().contains("mads")) {
+            return "I don't think so tbh lol";
+        }
+
+        return "BIAAAAAAAAAAAAAAAAATCH";
     }
 }
